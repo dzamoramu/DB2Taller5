3dee5cffc4221bd0807bfec5cd4037cddce6ca0e
DanielHurtado-040801 <89228676+DanielHurtado-040801@users.noreply.github.com>
2021-11-14 09:31:02 -0600

    conexion base de datos

---
+++ neo4j-tutorial/.ipynb_checkpoints/movies-checkpoint.py
@@ -0,0 +1,141 @@
+#!/usr/bin/env python
+import os
+import logging
+from json import dumps
+
+from flask import Flask, g, Response, request
+from neo4j import GraphDatabase, basic_auth
+
+app = Flask(__name__, static_url_path = "/static/")
+
+# Try to load database connection info from environment
+url = os.getenv("NEO4J_URI", "bolt://localhost:7687")
+username = os.getenv("NEO4J_USER", "neo4j2")
+password = os.getenv("NEO4J_PASSWORD", "neo4j2")
+neo4jVersion = os.getenv("NEO4J_VERSION", "4")
+database = os.getenv("NEO4J_DATABASE", "neo4j")
+port = os.getenv("PORT", 8080)
+
+# Create a database driver instance
+driver = GraphDatabase.driver(url, auth = basic_auth(username, password))
+
+# Connect to database only once and store session in current context
+def get_db():
+    if not hasattr(g, "neo4j_db"):
+        if neo4jVersion.startswith("4"):
+            g.neo4j_db = driver.session(database = database)
+        else:
+            g.neo4j_db = driver.session()
+    return g.neo4j_db
+
+# Close database connection when application context ends
+@app.teardown_appcontext
+def close_db(error):
+    if hasattr(g, "neo4j_db"):
+        g.neo4j_db.close()
+
+
+def serialize_movie(movie):
+    return {
+        'id': movie['id'],
+        'title': movie['title'],
+        'summary': movie['summary'],
+        'released': movie['released'],
+        'duration': movie['duration'],
+        'rated': movie['rated'],
+        'tagline': movie['tagline'],
+        'votes': movie.get('votes', 0)
+    }
+
+
+def serialize_cast(cast):
+    return {
+        'name': cast[0],
+        'job': cast[1],
+        'role': cast[2]
+    }
+
+
+@app.route("/")
+def get_index():
+    return app.send_static_file('index.html')
+
+
+@app.route("/graph")
+def get_graph():
+    db = get_db()
+    results = db.read_transaction(lambda tx: list(tx.run("MATCH (m:Movie)<-[:ACTED_IN]-(a:Person) "
+                                                         "RETURN m.title as movie, collect(a.name) as cast "
+                                                         "LIMIT $limit", {
+                                                             "limit": request.args.get("limit",
+                                                                                       100)})))
+    print(results)
+    
+    nodes = []
+    rels = []
+    i = 0
+    for record in results:
+        nodes.append({"title": record["movie"], "label": "movie"})
+        target = i
+        i += 1
+        for name in record['cast']:
+            actor = {"title": name, "label": "actor"}
+            try:
+                source = nodes.index(actor)
+            except ValueError:
+                nodes.append(actor)
+                source = i
+                i += 1
+            rels.append({"source": source, "target": target})
+    return Response(dumps({"nodes": nodes, "links": rels}),
+                    mimetype="application/json")
+
+
+@app.route("/search")
+def get_search():
+    try:
+        q = request.args["q"]
+    except KeyError:
+        return []
+    else:
+        db = get_db()
+        results = db.read_transaction(lambda tx: list(tx.run("MATCH (movie:Movie) "
+                                                             "WHERE movie.title =~ $title "
+                                                             "RETURN movie", {"title": "(?i).*" + q + ".*"}
+                                                             )))
+        return Response(dumps([serialize_movie(record['movie']) for record in results]),
+                        mimetype="application/json")
+
+
+@app.route("/movie/<title>")
+def get_movie(title):
+    db = get_db()
+    result = db.read_transaction(lambda tx: tx.run("MATCH (movie:Movie {title:$title}) "
+                                                   "OPTIONAL MATCH (movie)<-[r]-(person:Person) "
+                                                   "RETURN movie.title as title,"
+                                                   "COLLECT([person.name, "
+                                                   "HEAD(SPLIT(TOLOWER(TYPE(r)), '_')), r.roles]) AS cast "
+                                                   "LIMIT 1", {"title": title}).single())
+
+    return Response(dumps({"title": result['title'],
+                           "cast": [serialize_cast(member)
+                                    for member in result['cast']]}),
+                    mimetype="application/json")
+
+
+@app.route("/movie/<title>/vote", methods=["POST"])
+def vote_in_movie(title):
+    db = get_db()
+    summary = db.write_transaction(lambda tx: tx.run("MATCH (m:Movie {title: $title}) "
+                                                    "WITH m, (CASE WHEN exists(m.votes) THEN m.votes ELSE 0 END) AS currentVotes "
+                                                    "SET m.votes = currentVotes + 1;", {"title": title}).consume())
+    updates = summary.counters.properties_set
+
+    db.close()
+
+    return Response(dumps({"updates": updates}), mimetype="application/json")
+
+
+if __name__ == '__main__':
+    logging.info('Running on port %d, database is at %s', port, url)
+    app.run(port=port)
